5320152d76915722a0b8301fbfb7a1a9b883e63d
Philip Johnson <johnson@hawaii.edu>
2013-05-03 13:08:22 -1000

    Initial commit of project files.

---
+++ project/Build.scala
@@ -0,0 +1,23 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "play-example-bootstrap"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    javaCore,
+    javaJdbc,
+    javaEbean,
+    "org.webjars" % "webjars-play" % "2.1.0",
+    "org.webjars" % "bootstrap" % "2.3.1"
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
