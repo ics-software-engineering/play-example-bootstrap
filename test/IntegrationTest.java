20b7ba232051801cb92f6db5e2f82d37487dd28e
Philip Johnson <johnson@hawaii.edu>
2013-07-24 22:04:01 -1000

    Trying to use jquery 1.8.3, unsuccessfully

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -7,6 +7,7 @@ import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.testServer;
 import static play.test.Helpers.running;
 import static org.fest.assertions.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
 
 public class IntegrationTest {
     /**
@@ -14,7 +15,8 @@ public class IntegrationTest {
      */   
     @Test
     public void test() {
-        running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
+      assertTrue(true);
+        running(testServer(3333, fakeApplication()), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
                 browser.goTo("http://localhost:3333");
                 assertThat(browser.pageSource()).contains("Twitter Bootstrap");
